75e19b52b8b365852ffe6b02ccb3d90d0dd0bd8d
andreashgk <andreas@hgk.me>
2024-10-27 13:01:47 +0100

    Refactor `StatusIndicator` component

--- ags/bar/widgets/status-indicators.ts
+++ ags/bar/widgets/status-indicators.ts
@@ -1,16 +1,28 @@
 import { type Icon } from "lib/types";
-
 import { BarWidget } from "../bar-widget.js";
+import { Binding } from "types/service.js";
+import Gtk from "types/@girs/gtk-3.0/gtk-3.0.js";
 
 const audio = await Service.import('audio');
 const battery = await Service.import('battery')
 const bluetooth = await Service.import('bluetooth');
 const network = await Service.import('network');
 
-const StatusIndicator = (icon: Icon) => Widget.Icon({
-  size: 17,
-  className: "status-indicator",
-  icon,
+/** An icon giving some information about the current state of the system. */
+const StatusIndicator = (props: {
+  icon: Icon,
+  visible?: Binding<any, any, boolean>,
+  children?: Gtk.Widget[],
+}) => Widget.Box({
+  visible: props.visible ?? true,
+  children: [
+    Widget.Icon({
+      size: 17,
+      className: "status-indicator",
+      icon: props.icon,
+    }),
+    ...(props.children ?? []),
+  ],
 });
 
 export const StatusIndicators = () => {
@@ -21,44 +33,47 @@ export const StatusIndicators = () => {
     child: Widget.Box({
       className: "status-indicators",
       children: [
-        StatusIndicator(Utils.merge(
-          [network.bind('primary'), network.wired.bind('icon_name'), network.wifi.bind('icon_name')],
-          (primary, wired, wifi) => {
-            if (primary == "wifi") {
-              return wifi;
-            } else {
-              return wired;
+        StatusIndicator({
+          icon: Utils.merge(
+            [network.bind('primary'), network.wired.bind('icon_name'), network.wifi.bind('icon_name')],
+            (primary, wired, wifi) => {
+              if (primary == "wifi") {
+                return wifi;
+              } else {
+                return wired;
+              }
             }
-          }
-        )),
+          ),
+        }),
 
-        Widget.Box({
-          child: StatusIndicator("bluetooth-active-symbolic"),
+        StatusIndicator({
+          icon: "bluetooth-active-symbolic",
           visible: bluetooth.bind("connected_devices").as(devices => devices.length > 0),
         }),
 
-        StatusIndicator(audio.speaker.bind('volume').as((volume) => {
-          const icon = [
-            [101, 'overamplified'],
-            [67, 'high'],
-            [34, 'medium'],
-            [1, 'low'],
-            [0, 'muted'],
-          ].find(([threshold]) => (+threshold) <= (volume * 100))?.[1];
+        StatusIndicator({
+          icon: audio.speaker.bind('volume').as((volume) => {
+            const icon = [
+              [101, 'overamplified'],
+              [67, 'high'],
+              [34, 'medium'],
+              [1, 'low'],
+              [0, 'muted'],
+            ].find(([threshold]) => (+threshold) <= (volume * 100))?.[1];
 
-          return `audio-volume-${icon}-symbolic`;
-        })),
+            return `audio-volume-${icon}-symbolic`;
+          }),
+        }),
 
-        StatusIndicator(Utils.merge(
-          [battery.bind('available'), battery.bind('icon_name')],
-          (enabled, icon_name) => {
-            if (!enabled) {
-              return "system-shutdown-symbolic";
-            }
+        StatusIndicator({
+          icon: battery.bind("icon_name"),
+          visible: battery.bind("available"),
+        }),
 
-            return icon_name;
-          }
-        )),
+        StatusIndicator({
+          icon: "system-shutdown-symbolic",
+          visible: battery.bind("available").as(avail => !avail),
+        }),
       ],
     }),
     onClicked: () => App.toggleWindow('quicksettings'),
